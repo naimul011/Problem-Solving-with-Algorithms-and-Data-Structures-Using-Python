1e08d15c43e846a550c0862031e508b28705e27d
Emma Paris <naimul011@gmail.com>
2020-03-25 02:11:21 +0600

    Arithmetic, Relational and Logical Operators

---
+++ README.md
@@ -0,0 +1,1 @@
+# naimul011-Problem-Solving-with-Algorithms-and-Data-Structures-Using-Python
---
+++ test.py
@@ -0,0 +1,34 @@
+print(2+3*4) #14
+print((2+3)*4) #20
+print(2**10) #1024
+print(6/3) #2.0
+print(7/3) #2.33333333333
+print(7//3) #2
+print(7%3) #1
+print(3/6) #0.5
+print(3//6) #0
+print(3%6) #3
+print(2**100) # 1267650600228229401496703205376
+
+>>> True
+True
+>>> False
+False
+>>> False or True
+True
+>>> not (False or True)
+False
+>>> True and True
+True
+
+>>> the_sum = 0
+>>> the_sum
+0
+>>> the_sum = the_sum + 1
+>>> the_sum
+1
+>>> the_sum = True
+>>> the_sum
+True
+
+
