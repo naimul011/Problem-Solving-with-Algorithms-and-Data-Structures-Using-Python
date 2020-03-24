16e2947d5b9645bf6e0217c4e50b57b9feab8909
Emma Paris <naimul011@gmail.com>
2020-03-25 03:12:19 +0600

    List Comprehension

--- test.py
+++ test.py
@@ -189,4 +189,23 @@ if n < 0:
 	n = abs(n)
 print(math.sqrt(n))
 
+# List comprehension
+
+>>> sq_list = []
+>>> for x in range(1, 11):
+	sq_list.append(x * x)
+>>> sq_list
+[1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+>>>
+
+>>> sq_list = [x * x for x in range(1, 11)]
+>>> sq_list
+[1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+>>>
+
+>>> sq_list = [x * x for x in range(1, 11) if x % 2 != 0]
+>>> sq_list
+[1, 9, 25, 49, 81]
+>>>
+
 
